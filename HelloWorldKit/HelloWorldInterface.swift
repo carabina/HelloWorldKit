b0cad79dbb1fcdbe48aef1e3ce3d4fe3bc3a7be1
Bryan Yeung <Bryan@Bryans-MacBook-Pro.local>
2016-12-08 10:41:43 +0800

    fasjkld

--- HelloWorldKit/HelloWorldInterface.swift
+++ HelloWorldKit/HelloWorldInterface.swift
@@ -5,8 +5,6 @@
 //  Created by Bryan Yeung on 7/12/2016.
 //  Copyright © 2016 Bryan Yeung. All rights reserved.
 //
-<<<<<<< HEAD
-
 import Foundation
 
 public class HelloWorldInterface {
@@ -14,14 +12,4 @@ public class HelloWorldInterface {
    public func showView(vc: UIViewController) {
       vc.present(ViewController(), animated: true, completion: nil);
    }
-   
-=======
-import Foundation
-
-public class HelloWorldInterface {
-   class func getHelloWorldViewController (vc: UIViewController) {
-      let controller = HelloWorldViewController()
-      vc.present(controller, animated: true, completion: nil)
-   }
->>>>>>> 7d54cf507d7700252e437c23921e209bd82ab898
 }
